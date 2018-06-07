63bbe3f1143ad338e8c68ae38089e93dbe78cba4
David Zernik <dzernik@Davids-MacBook-Pro-2.local>
2018-06-07 14:39:31 -0700

    working with hardcoded repo data.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
+  var dataHardcoded = {states: [
+      {name:"Pennsylvania"},
+      {name:"NJ"},
+      {name:"Florida"},
+      {name:"California"},
+      {name:"Texas"},
+      {name:"New Mexico"},
+      {name:"Arizona"},
+      {name:"Maine"},
+      {name:"NH"},
+      {name:"Montana"},
+      {name:"Oklahoma"}
+  ]};
+
+  var reposHardcoded = {repositories: [
+      {
+        created:"2012-02-01T13:52:00Z",
+        created_at: "2012-02-01T13:52:00Z"
+      },
+      {name:"NJ"},
+      {name:"Florida"},
+      {name:"California"},
+      {name:"Texas"},
+      {name:"New Mexico"},
+      {name:"Arizona"},
+      {name:"Maine"},
+      {name:"NH"},
+      {name:"Montana"},
+      {name:"Oklahoma"}
+  ]};
+
   var username;
   $('.loader-container').hide();
+
+  // inside click function
+  // -----------------------------------------------------------------------------
   $("#search").on("click", function() {
     var searchTerm = $('#searchTerm').val();
     var url =  "https://api.github.com/legacy/repos/search/";
@@ -12,13 +46,24 @@ $(document).ready(function () {
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       success: function(data, status, jqXHR) {
+        var dataComingBack;
         console.log(data);
-        console.log('stringified', JSON.stringify(data.repositories[0]["username"]));
+        console.log('data stringified', JSON.stringify(data.repositories[0]["username"]));
         username = JSON.stringify(data.repositories[0]["username"]);
+        dataComingBack = JSON.stringify(data);
+        // console.log('data coming back', dataComingBack.repositories);
+
         // $("#results").html();
         $("#results").toggleClass('hidden');
 
         $("#results").prepend(username);
+
+        // variable for script it shows up on page
+        var source = $("#myTemplate").html();
+        console.log('reposHardcoded here', username);
+        var template = Handlebars.compile(source);
+        var html = template(reposHardcoded);
+        $("#stateList").html(html);
 
       }
     })
@@ -26,29 +71,6 @@ $(document).ready(function () {
       console.log("success");
     })
   });
-
-// Handlebars templating
-// -----------------------------------------------------------------------------
-    var data = {states: [
-        {name:"Pennsylvania"},
-        {name:"NJ"},
-        {name:"Florida"},
-        {name:"California"},
-        {name:"Texas"},
-        {name:"New Mexico"},
-        {name:"Arizona"},
-        {name:"Maine"},
-        {name:"NH"},
-        {name:"Montana"},
-        {name:"Oklahoma"}
-    ]};
-
-    // variable for script it shows up on page
-    var source = $("#myTemplate").html();
-    //
-    var template = Handlebars.compile(source);
-    var html = template(username);
-    $("#stateList").html(html);
 });
 
 // Stop and start for loader image
