6b285e7a53d71e8d996943de61d9becab6c82997
David Zernik <dzernik@Davids-MacBook-Pro-2.local>
2018-06-07 22:47:15 -0700

    tweaks and such

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 $(document).ready(function () {
   $('.name').on('click', function() {
-    console.log('clicked name!!!');
-        if ($(this).next().is(':hidden')) {
-          $(this).next().slideDown();
-          $(this).css('color', '#E03616');
+    if ($(this).next().is(':hidden')) {
+      $(this).next().slideDown();
+      $(this).css('color', '#E03616');
 
-        } else {
-          console.log('worked name!!!');
-
-            $(this).next().slideUp();
-            $(this).css('color', 'black');
-        }
+    } else {
+      $(this).next().slideUp();
+      $(this).css('color', 'black');
+    }
   });
 
+  // Variable where cache will be stored
   var requestCache = {};
+  // Basic endpoint without search query
   var url =  "https://api.github.com/legacy/repos/search/";
 
-  function cacheOrApiCall(searchTerm) {
-    if (!requestCache[searchTerm]) {
-      console.log('not cached!!!');
+  function getCacheOrMakeRequest(searchTerm) {
+    if (requestCache[searchTerm]) {
+      console.log('IT IS cached!');
+    } else {
+      console.log('not cached...make request!');
       requestCache[searchTerm] =
       $.ajax({
         url: url + searchTerm,
@@ -26,29 +27,24 @@ $(document).ready(function () {
         contentType: "application/json; charset=utf-8",
         dataType: "json"
       });
-    } else {
-      console.log('NOW ITS cached!');
     }
 
     return requestCache[searchTerm];
   }
 
-  // inside click function
-  // ------------------------------------------------------------
-  $("#search").on("click", function() {
+  $('#search').on('click', function() {
     var searchTerm = $('#searchTerm').val();
 
-    cacheOrApiCall(searchTerm)
+    getCacheOrMakeRequest(searchTerm)
+      // returns promise, now manipulate data
       .then(function(data, status, jqXHR) {
-        // variable for script it shows up on page
         var source = $("#myTemplate").html();
         var template = Handlebars.compile(source);
         var html = template(data);
 
-        console.log('data', data);
-        $("#stateList").html(html);
+        $("#listForPagination").html(html);
 
-        var monkeyList = new List('test-list', {
+        var paginationItems = new List('test-list', {
           valueNames: ['name'],
           page: 10,
           pagination: true
@@ -56,18 +52,16 @@ $(document).ready(function () {
 
         $('.category__extra-info-container').hide();
     })
-  });  // closes click event
+  });
 });
 
 // This event needs to bound to static element in order to work with pagination
 $(document).on('click', "li", function () {
-  console.log('harry!');
   $('.category__extra-info-container').hide();
 });
 
 // This event also needs to bound to static element in order to work with pagination
 $(document).on('click', '.name', function () {
-  console.log('clicked name!!!');
       if ($(this).next().is(':hidden')) {
         $(this).next().slideDown();
         $(this).css('color', '#eaeaea');
@@ -77,6 +71,7 @@ $(document).on('click', '.name', function () {
           $(this).css('color', 'black');
       }
 });
+
 // Stop and start for loader image
 // -----------------------------------------------------------------------------
 $(document).ajaxStop(function () {
